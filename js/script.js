3fe086b30ebd3d84b44223173b3f84026f01fe5c
kristelleppmets <63917482+kristelleppmets@users.noreply.github.com>
2020-04-18 18:39:15 +0200

    Change Title

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+Hello
+ * `quotes` array
 ***/
 
 
